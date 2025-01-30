7ebf2126c231073359bf3bb976c1d7c12b22a5e1
elsilv <elsi.silvennoinen@gmail.com>
2025-01-30 12:13:52 +0200

    Add left-side navbar for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HorseStorePage from './pages/HorseStorePage';
 import LandingPage from "./pages/LandingPage.jsx";
 import { useSelector } from "react-redux";
+import NavBar from "./components/Navbar.jsx";
 
 function App() {
   const horses = useSelector((state) => state.horses.ownedHorses);
 
   return (
       <Router>
+        <NavBar />
         <Routes>
           <Route path="/" element={<LandingPage horses={horses} />} />
           <Route path="/horse-store" element={<HorseStorePage />} />
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../index.css';
+
+const NavBar = () => {
+  return (
+    <nav className="sidebar">
+      <h2>🏇 Riding School</h2>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/horses">Horses</Link></li>
+        <li><Link to="/lessons">Lessons</Link></li>
+        <li><Link to="/bills">Bills</Link></li>
+        <li><Link to="/horse-store">Horse Store</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
